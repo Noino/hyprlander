30e46fdb2aea2cf5749e698e53e3980d430c95a1
Noino <willewendell@gmail.com>
2023-06-16 00:06:06 +0300

    commant

--- install.sh
+++ install.sh
@@ -7,12 +7,15 @@ src_dir=`pwd`;
 # source bash configs at end of profile
 [[ `rg "${src_dir##*/}" ~/.bash_profile` ]] || echo "[[ -f \"${src_dir}\"/bash_configs ]] && . \"${src_dir}\"/bash_configs" >> ~/.bash_profile
 
-
+# makes linking ez
 cd ~/.config
 
 for d in ${src_dir}/dotconfig/*; do 
+	# backup exiting things
 	[[ -e "${d##*/}" && ! -L "${d##*/}" ]] && mv "${d##*/}" "${d##*/}".bak
+	# remove existing links
 	[[ -L "${d##*/}" ]] && rm "${d##*/}"
+	# make install
 	[[ -d $d ]] && ln -s "$d/" || ln -s "$d" 
 done
 
