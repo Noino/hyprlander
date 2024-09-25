c9081f36bdb5c4bd12a0e27f6af716744219a808
Noino <willewendell@gmail.com>
2024-09-25 13:49:28 +0300

    tray icons

--- dotconfig/eww/modules/sound/speaker.sh
+++ dotconfig/eww/modules/sound/speaker.sh
@@ -2,25 +2,25 @@
 
 case $1 in
       "*") command="" ;;
-      "up") command="--allow-boost -i 5" ;;
-      "down") command="--allow-boost -d 5" ;;
+      "up") command="--allow-boost -i 1" ;;
+      "down") command="--allow-boost -d 1" ;;
       "toogle") command="-t" ;;
 esac
 
-[ -n "$command" ] && pamixer $command 
+[ -n "$command" ] && pamixer $command
 mute=$(pamixer --get-mute)
 if [ "$mute" = "true" ]; then
       volume="muted"
       icon=""
-else 
+else
       volume="$(pamixer --get-volume)"
       if [ "$volume" -gt 66 ]; then
             icon=""
       elif [ "$volume" -gt 33 ]; then
             icon=""
-      elif [ "$volume" -gt 0 ]; then 
+      elif [ "$volume" -gt 0 ]; then
             icon=""
-      else 
+      else
             icon=""
       fi
       volume="$volume%"
