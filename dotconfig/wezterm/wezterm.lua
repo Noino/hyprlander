aec324e58cc15214c56e85ef85f8a589d7aa38d2
Noino <willewendell@gmail.com>
2025-03-18 14:44:12 +0200

    little fixes here and there, finally working tmux selector with the utility_mode flag

--- dotconfig/wezterm/wezterm.lua
+++ dotconfig/wezterm/wezterm.lua
@@ -8,7 +8,7 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+    config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
@@ -21,26 +21,25 @@ config.font = wezterm.font('JetBrains Mono')
 config.window_background_opacity = 0.85
 config.hide_tab_bar_if_only_one_tab = true
 config.audible_bell = 'Disabled'
-enable_kitty_keyboard = true
+config.enable_kitty_keyboard = true
 config.enable_wayland = false
 
 
 config.mouse_bindings = {
-	{
-		event = { Down = { streak = 1, button = "Right" } },
-		mods = "NONE",
-		action = wezterm.action_callback(function(window, pane)
-			local has_selection = window:get_selection_text_for_pane(pane) ~= ""
-		 	if has_selection then
-		 		window:perform_action(act.CopyTo("ClipboardAndPrimarySelection"), pane)
-		 		window:perform_action(act.ClearSelection, pane)
-		 	else
-		 		window:perform_action(act({ PasteFrom = "Clipboard" }), pane)
-		 	end
-		 end),
-	},
+    {
+        event = { Down = { streak = 1, button = "Right" } },
+        mods = "NONE",
+        action = wezterm.action_callback(function(window, pane)
+            local has_selection = window:get_selection_text_for_pane(pane) ~= ""
+            if has_selection then
+                window:perform_action(act.CopyTo("ClipboardAndPrimarySelection"), pane)
+                window:perform_action(act.ClearSelection, pane)
+            else
+                window:perform_action(act({ PasteFrom = "Clipboard" }), pane)
+            end
+        end),
+    },
 }
 
 -- and finally, return the configuration to wezterm
 return config
-
