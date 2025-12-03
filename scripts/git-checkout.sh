12ba2e79f2698e2df32db65f0755f73d9c18473b
Noino <willewendell@gmail.com>
2025-12-03 11:29:49 +0200

    fix

--- scripts/git-checkout.sh
+++ scripts/git-checkout.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-
+gco() {
 [[ -n "$(git status 2>/dev/null)" ]] && {
     [[ $# -gt 0 ]] && { git checkout "$@"; return; }
     git checkout ${ git branch -a | perl -pe 's~[* ]*(remotes/origin/)?~~' | sort -u | fzf ; }
 } || echo "Not a git repository"
+}
+gco "$@"
