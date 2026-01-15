279defbf01aae5e82aecd48249e3f803d0852177
Noino <willewendell@gmail.com>
2026-01-15 08:47:53 +0200

    autocommit

--- scripts/start-keepass.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-keepassxc &
-pid=$!
-
-while ! hyprctl clients | grep -E "KeePassXC"; do sleep 0.1; done
-hyprctl dispatch movetoworkspacesilent special:keepass,'initialclass:org.keepassxc.KeePassXC'
